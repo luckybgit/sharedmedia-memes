82e44c2cbdd23b7c0a11f1f4e36c5cfccc8481f1
luckybgit <37333947+luckybgit@users.noreply.github.com>
2022-02-25 14:59:22 +0100

    Add files via upload

---
+++ Soundpaths.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+
+LSM:Register("sound", "|cFF65C7FFBronCena|r", [[Interface\Addons\SharedMedia_Memes\sound\broncena.ogg]])
+LSM:Register("sound", "|cFF65C7FFAirHorn|r", [[Interface\Addons\SharedMedia_Memes\sound\airhorn.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
